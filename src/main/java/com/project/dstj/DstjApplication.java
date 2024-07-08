8e182697a6218ccba49818e0e79043277ca35ccd
novzs <novzs@naver.com>
2024-07-08 17:01:11 +0900

    Update main class and add dotenv configuration

--- src/main/java/com/project/dstj/DstjApplication.java
+++ src/main/java/com/project/dstj/DstjApplication.java
@@ -2,11 +2,14 @@ package com.project.dstj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DstjApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().load();
 		SpringApplication.run(DstjApplication.class, args);
 	}
 

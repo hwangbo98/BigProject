3bfa20ae1e5fdac57a4350e057f0cde2461b1ac4
moheemohaeng <moheemohaeng@gmail.com>
2024-07-17 03:26:20 +0900

    셀프 탈퇴(관리자, 직원, 회원 모두가능) 기능 구현 del_user

--- src/main/java/com/project/dstj/controller/DelUserController.java
+++ src/main/java/com/project/dstj/controller/DelUserController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.dstj.entity.Alluser;
+import com.project.dstj.repository.AlluserRepository;
 import com.project.dstj.request.DelUserRequest;
+import com.project.dstj.security.JwtTokenProvider;
 import com.project.dstj.service.DelUserService;
 
 @RestController
@@ -14,10 +18,20 @@ public class DelUserController {
     @Autowired
     private DelUserService delUserService;
     
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AlluserRepository alluserRepository;
+
+    
     @PostMapping(value = "/del_user", consumes = "application/json")
-    public ResponseEntity<Void> delUser(@RequestBody DelUserRequest request){
-        String username = request.getUsername();
-        delUserService.deleteUser(username);
+    public ResponseEntity<Void> delUser(@RequestHeader("Authorization") String token){
+        token = token.substring(7);
+        String loginUser = jwtTokenProvider.getUsernameFromJWT(token);
+        Alluser user = alluserRepository.findByUsername(loginUser)
+        .orElseThrow(() -> new RuntimeException("User not found"));
+        delUserService.deleteUser(user.getUsername());
         return ResponseEntity.ok().build();
     }
 

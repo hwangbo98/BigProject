3536a60c7b186603c39cf1ca4a3400814e32fe4b
mini0-0 <nalala8200@naver.com>
2024-07-16 22:31:07 +0900

    member report ìãìreport 이름, Test테이블정보 조회

--- src/main/java/com/project/dstj/repository/TestRepository.java
+++ src/main/java/com/project/dstj/repository/TestRepository.java
@@ -2,7 +2,7 @@ package com.project.dstj.repository;
 
 import com.project.dstj.entity.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,14 +11,6 @@ import java.util.List;
 public interface TestRepository extends JpaRepository<Test, Long> {
     List<Test> findByTakes_Member_Alluser_Place_PlacePKAndTakes_Member_MemberPK(Long placePK, Long memberPK);
 
-
-
-
-=======
-
-import java.util.List;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
     List<Test> findByTakesMemberAlluserUserPK(Long userPK);
->>>>>>> 3966cad48f9c50af961c9cb3c0e96e00e1b49016
+
 }

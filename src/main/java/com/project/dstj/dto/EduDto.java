8f3d276a80664e496f2ac975bcef3183cb90f3ca
mini0-0 <nalala8200@naver.com>
2024-07-23 21:51:10 +0900

    edu-list 수정

--- src/main/java/com/project/dstj/dto/EduDto.java
+++ src/main/java/com/project/dstj/dto/EduDto.java
@@ -1,10 +1,14 @@
 package com.project.dstj.dto;
 
+import com.project.dstj.entity.Edu;
+import com.project.dstj.entity.Worker;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 public class EduDto {
     private Long eduPK;
     private String eduDay;
@@ -15,4 +19,18 @@ public class EduDto {
     private Long workerPK;
     private String userNickname;
     private String username;
+
+    public static EduDto toDto(Edu edu) {
+        EduDto eduDto = new EduDto();
+        eduDto.setEduPK(edu.getEduPK());
+        eduDto.setEduDay(edu.getEduDay());
+        eduDto.setEduEnd(edu.getEduEnd());
+        eduDto.setEduName(edu.getEduName());
+        eduDto.setEduStart(edu.getEduStart());
+        eduDto.setPlacePK(edu.getPlace().getPlacePK());
+        eduDto.setWorkerPK(edu.getWorker().getWorkerPK());
+        eduDto.setUserNickname(edu.getWorker().getAlluser().getUserNickname());
+        eduDto.setUsername(edu.getWorker().getAlluser().getUsername());
+        return eduDto;
+    }
 }
--- src/main/java/com/project/dstj/service/EduInfoService.java
+++ src/main/java/com/project/dstj/service/EduInfoService.java
@@ -19,27 +19,15 @@ public class EduInfoService {
     private final EduRepository eduRepository;
     private final WorkerRepository workerRepository;
 
-    // 3. Alluser에서 사용자의 placePK를 파악하는 메서드
     public Long getPlacePKByUsername(String username) {
         return alluserRepository.findByUsername(username)
                 .map(alluser -> alluser.getPlace().getPlacePK())
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
-    // 4. Alluser, Worker, Edu를 join하고 placePk가 같은 값들만 추출하는 메서드
     public List<EduDto> getEduListByPlacePK(Long placePK) {
         return eduRepository.findEduByPlacePK(placePK).stream()
-                .map(edu -> new EduDto(
-                        edu.getEduPK(),
-                        edu.getEduDay(),
-                        edu.getEduEnd(),
-                        edu.getEduName(),
-                        edu.getEduStart(),
-                        edu.getPlace().getPlacePK(),
-                        edu.getWorker().getWorkerPK(),
-                        edu.getWorker().getAlluser().getUserNickname(),
-                        edu.getWorker().getAlluser().getUsername()
-                ))
+                .map(EduDto::toDto)
                 .collect(Collectors.toList());
     }
 }

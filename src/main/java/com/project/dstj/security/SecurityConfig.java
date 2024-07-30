e47cee41f3329d37d1178d75d460d7fc83faea56
mini0-0 <nalala8200@naver.com>
2024-07-30 17:29:47 +0900

    securityconfig update

--- src/main/java/com/project/dstj/security/SecurityConfig.java
+++ src/main/java/com/project/dstj/security/SecurityConfig.java
@@ -28,15 +28,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/**").permitAll()
-                                .anyRequest().permitAll()
-//                        .requestMatchers("/auth/sign-up").permitAll()
-//                        .anyRequest().authenticated()
+                        .requestMatchers("/auth/sign-up", "/auth/sign-in").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-
-                // 추가된 부분: CORS 설정 적용
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())); // CORS 설정 추가
 
         return http.build();
@@ -44,14 +40,11 @@ public class SecurityConfig {
 
     //CORS 설정 추가
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() { // CORS 설정 추가
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 허용할 도메인 설정
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://fasthrm.s3-website.ap-northeast-2.amazonaws.com")); // 허용할 도메인 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드 설정
-        //configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 허용할 헤더 설정
-        //configuration.addAllowedHeader("*");
         configuration.setAllowedHeaders(Arrays.asList("*")); // 허용할 HTTP 헤더 설정
-        //configuration.allowedHeaders("*");
         configuration.setAllowCredentials(true); // 자격 증명 허용 설정
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
